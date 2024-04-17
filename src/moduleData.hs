afa6d0115a9328adae24ab94050bfb18bb0e44c7
Jack Duggan <74904632+jackjduggan@users.noreply.github.com>
2024-04-17 18:53:09 +0100

    Defined FromNamedRecord instance

--- src/moduleData.hs
+++ src/moduleData.hs
@@ -5,6 +5,8 @@ import Data.Text (Text)
 import qualified Data.Vector as V
 import GHC.Generics (Generic)
 
+-- Step 1: Define a model
+
 data Module = Module
     { code :: Text
     , fullTitle :: Text
@@ -17,3 +19,20 @@ data Module = Module
     , learningOutcomes :: Text
     , assessmentCriteria :: Text
     } deriving (Show, Generic)
+
+-- Step 2: Parse CSV
+
+-- Define FromNamedRecord instance for Student
+instance FromNamedRecord Module where
+    parseNamedRecord r = do
+        code'               <- r .: "Code"
+        fullTitle'          <- r .: "Full_Title"
+        shortTitle'         <- r .: "Short_Title"
+        credits'            <- r .: "Credits"
+        level'              <- r .: "Level"
+        aim'                <- r .: "Aim"
+        department'         <- r .: "Department"
+        indicativeContent'  <- r .: "Indicative_Content"
+        learningOutcomes'   <- r .: "Learning_Outcomes"
+        assessmentCriteria' <- r .: "Assessment_Criteria"
+        return $ Module code' fullTitle' shortTitle' credits' level' aim' department' indicativeContent' learningOutcomes' assessmentCriteria'
