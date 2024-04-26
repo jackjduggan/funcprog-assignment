0a8ae364435f01000341cbcfa038816f99551519
Jack Duggan <74904632+jackjduggan@users.noreply.github.com>
2024-04-26 15:58:15 +0100

    validateFullTitle function added- still need to check for uniqueness

--- src/ValidateModule.hs
+++ src/ValidateModule.hs
@@ -9,7 +9,7 @@ import ModuleData (Module(..), ValidatedModule(..))
 --                  | Takes a module from the first datatype
 --                  |  and runs the validations across it, returning the
 --                  V  validated module in form of second datatype.
-ValidateModule :: Module -> ValidatedModule
+validateModule :: Module -> ValidatedModule
 validateModule m = ValidatedModule
     { validatedCode = validateCode (code m)
     , validatedFullTitle = validateFullTitle 
@@ -33,14 +33,19 @@ validateCode code
 
 validateFullTitle :: String -> Either String String
 validateFullTitle fullTitle
-    | -- TODO each word should have first letter capitalized
-    | -- TODO must be unique in dataset
+    | not (all isTitleCase wordsList) = Left "Must be in title case"
+    -- | TODO: handle uniqueness
     | otherwise = Right fullTitle
+  where
+    wordsList = words fullTitle
+    isTitleCase :: String -> Bool
+    isTitleCase [] = False -- Empty string automatically false
+    isTitleCase (x:xs) = isUpper x && all isLower xs -- First char upper, rest lower
 
 validateShortTitle :: String -> Either String String
 validateShortTitle shortTitle
     | length shortTitle > 30 = Right "Must have max of 30 characters"
-    | -- TODO should be equal to Full_Title if Full_title is 30 chars or less
+    -- | TODO should be equal to Full_Title if Full_title is 30 chars or less
     | otherwise = Right shortTitle
 
 validateCredits :: Int -> Either String Int
@@ -59,25 +64,25 @@ validateAim aim
     | otherwise Right aim
 
 validateDepartment :: String -> Either String String
-validateDepartment department
+validateDepartment dep
     | -- if not one of Science, C&M, ET
-    | otherwise Right department
+    | otherwise Right dep
 
 validateIndicativeContent :: String -> Either String String
-validateIndicativeContent indicativeContent
+validateIndicativeContent ic
     | -- each sentence must begin with capital letter
-    | otherwise Right indicativeContent
+    | otherwise Right ic
 
 validateLearningOutcomes :: String -> Either String String
-validateLearningOutcomes learningOutcomes
+validateLearningOutcomes lo
     | -- number of outcomes dependant on module level
-    | otherwise Right learningOutcomes
+    | otherwise Right lo
 
 validateAssessmentCriteria :: String -> Either String String
-validateAssessmentCriteria assessmentCriteria
+validateAssessmentCriteria ac
     | -- if less than 4 categories (lines)
     | -- if less than 1 occurence of % in each lines
-    | otherwise Right assessmentCriteria
+    | otherwise Right ac
 
 -- References:
 -- ref1: "By doing Shape(..), we exported all the value constructors for Shape" https://learnyouahaskell.com/making-our-own-types-and-typeclasses
